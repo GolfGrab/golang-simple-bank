dbd3abdfbb665ad95a882408c7774360aed84104
GolfGrab <surapus.m@ku.th>
2022-01-26 01:38:35 +0700

    unit test not finished

--- Makefile
+++ Makefile
@@ -13,4 +13,10 @@ migrateup:
 migratedown:
 	migrate -path db/migration -database "postgresql://root:password@localhost:5432/simple_bank?sslmode=disable" -verbose down
 
-.PHONY: postgres createdb dropdb migrateup migratedown
+sqlc:
+	docker run --rm -v C:\Users\User\Desktop\Golf\journey-to-complete-backend:/src -w /src kjconroy/sqlc generate
+
+test:
+	go test -v -cover
+
+.PHONY: postgres createdb dropdb migrateup migratedown sqlc
---
+++ util/random.go
@@ -0,0 +1,42 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandomInt returns a random int in the range [min, max]
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// RandomString returns a random string of length n
+func RandomString(n int) string {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+// RandomOwner returns a random owner name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+// RandomMoney returns a random money amount
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+// RandomCurrency returns a random currency
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "CAD"}
+	n := rand.Intn(len(currencies))
+	return currencies[n]
+}
