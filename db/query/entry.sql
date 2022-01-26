8b753c48a8f62ff21fc940a3bfdaf65d6b85d7b0
GolfGrab <surapus.m@ku.th>
2022-01-26 10:38:01 +0700

    test transfer and entry CRUD func

--- db/query/entry.sql
+++ db/query/entry.sql
@@ -16,13 +16,3 @@ WHERE account_id = $1
 ORDER BY id
 LIMIT $2
 OFFSET $3;
-
--- -- name: UpdateEntry :one
--- UPDATE entries
--- SET amount = $2
--- WHERE id = $1
--- RETURNING *;
-
--- -- name: DeleteEntry :exec
--- DELETE FROM entries
--- WHERE id = $1;
