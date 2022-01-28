e10c0ff74f6c89963e2789f249d7903b4b2c8936
GolfGrab <surapus.m@ku.th>
2022-01-29 00:17:56 +0700

    create and verify JWT & PASETO

---
+++ token/paseto_maker_test.go
@@ -0,0 +1,60 @@
+package token
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/GolfGrab/journey-to-complete-backend/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPasetoMaker(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomOwner()
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, payload.Username, username)
+	require.WithinDuration(t, payload.IssuedAt, issuedAt, time.Second)
+	require.WithinDuration(t, payload.ExpiresAt, expiredAt, time.Second)
+}
+
+func TestExpiredPasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	token, err := maker.CreateToken(util.RandomOwner(), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
+
+func TestInvalidPasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	token := util.RandomString(32)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, errors.New("failed to decode token: incorrect token header").Error())
+	require.Nil(t, payload)
+}
