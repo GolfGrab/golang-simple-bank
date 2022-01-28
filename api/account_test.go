2bed18aa524340bfe10cb0a52a441bcc875c52cf
GolfGrab <surapus.m@ku.th>
2022-01-29 02:15:09 +0700

    implement login user api

--- api/account_test.go
+++ api/account_test.go
@@ -95,7 +95,7 @@ func TestGetAccountAPI(t *testing.T) {
 			store := mockdb.NewMockStore(ctrl)
 			tc.buildStubs(store)
 
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := fmt.Sprintf("/accounts/%d", tc.accountID)
@@ -185,7 +185,7 @@ func TestCreateAccountAPI(t *testing.T) {
 			store := mockdb.NewMockStore(ctrl)
 			tc.buildStubs(store)
 
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := "/accounts"
@@ -305,7 +305,7 @@ func TestListAccountsAPI(t *testing.T) {
 			store := mockdb.NewMockStore(ctrl)
 			tc.buildStubs(store)
 
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := "/accounts"
--- api/main_test.go
+++ api/main_test.go
@@ -3,9 +3,24 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
+	db "github.com/GolfGrab/journey-to-complete-backend/db/sqlc"
+	"github.com/GolfGrab/journey-to-complete-backend/util"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
+}
 
 // TestMain is the entry point for testing.
 func TestMain(m *testing.M) {
