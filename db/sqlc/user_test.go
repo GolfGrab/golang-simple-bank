f76f93439eca7b5d5f59caf54f087dcc7c38bb97
GolfGrab <surapus.m@ku.th>
2022-01-28 17:39:24 +0700

    impliment create user api and hash password bcrypt

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -10,9 +10,12 @@ import (
 )
 
 func CreateRandomUser(t *testing.T) User {
+	hashedPassword, err := util.HashPassword(util.RandomString(10))
+	require.NoError(t, err)
+
 	arg := CreateUserParams{
 		Username:       util.RandomOwner(),
-		HashedPassword: "secret",
+		HashedPassword: hashedPassword,
 		FullName:       util.RandomOwner(),
 		Email:          util.RandomEmail(),
 	}
